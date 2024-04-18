e44cbcff9cf0ac2cebcaff95132d8609a3a2f456
girija321 <130287289+girija321@users.noreply.github.com>
2024-04-18 14:13:48 -0700

    second push

---
+++ src/test/java/pom/ArrayPage.java
@@ -0,0 +1,8 @@
+package pom;
+
+import org.openqa.selenium.By;
+
+public class ArrayPage {
+	By GetStarted = By.xpath("//a[@href='array']");
+
+}
---
+++ src/test/java/stepDefination/ArraySteps.java
@@ -0,0 +1,37 @@
+package stepDefination;
+
+import io.cucumber.java.en.*;
+
+public class ArraySteps {
+	
+	@Given("I want to write a step with precondition")
+	public void i_want_to_write_a_step_with_precondition() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("some other precondition")
+	public void some_other_precondition() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I complete action")
+	public void i_complete_action() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("some other action")
+	public void some_other_action() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("yet another action")
+	public void yet_another_action() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+}
